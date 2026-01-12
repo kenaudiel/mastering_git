1ebcd177f5eae598206c12cf71b061ce1e0440c4
Kenneth <audielkenneth23@gmail.com>
2026-01-12 14:46:33 +0700

    add setter getter in Car

--- src/IVehicle.java
+++ src/IVehicle.java
@@ -2,6 +2,6 @@ public interface IVehicle {
     public void Running();
     public String getBrand();
     public void setBrand(String brand);
-    public void getPrice();
-    public void setPrice();
+    public double getPrice();
+    public void setPrice(double price);
 }
